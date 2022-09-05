0080b797d080c95a5b9d0dba4e20900ad2c124d8
Ryan Moore <ryan@geekportfolio.com>
2022-09-05 07:56:47 +0000

    testing optimizations from neonsecret

--- ldm/modules/attention.py
+++ ldm/modules/attention.py
@@ -149,23 +149,26 @@ class CrossAttention(nn.Module):
         context = default(context, x)
         k = self.to_k(context)
         v = self.to_v(context)
+        del context, x
 
         q, k, v = map(lambda t: rearrange(t, "b n (h d) -> (b h) n d", h=h), (q, k, v))
 
-        sim = einsum("b i d, b j d -> b i j", q, k) * self.scale
+        r1 = torch.zeros(q.shape[0], q.shape[1], v.shape[2])
+        for i in range(0, q.shape[0], 2):
+            q, k = q.cuda(), k.cuda()
+            s1 = einsum("b i d, b j d -> b i j", q[i : i + 2], k[i : i + 2])
+            q, k = q.cpu(), k.cpu()
+            s1 *= self.scale
 
-        if exists(mask):
-            mask = rearrange(mask, "b ... -> b (...)")
-            max_neg_value = -torch.finfo(sim.dtype).max
-            mask = repeat(mask, "b j -> (b h) () j", h=h)
-            sim.masked_fill_(~mask, max_neg_value)
+            s1[1:] = s1[1:].softmax(dim=-1)
+            s1[:1] = s1[:1].softmax(dim=-1)
 
-        # attention, what we cannot get enough of
-        attn = sim.softmax(dim=-1)
+            r1[i : i + 2] = einsum("b i j, b j d -> b i d", s1, v[i : i + 2]).cpu()
+        del s1
+        r2 = rearrange(r1.to(q.device), "(b h) n d -> b n (h d)", h=h).cuda()
+        del r1, q, k, v
 
-        out = einsum("b i j, b j d -> b i d", attn, v)
-        out = rearrange(out, "(b h) n d -> b n (h d)", h=h)
-        return self.to_out(out)
+        return self.to_out(r2)
 
 
 class BasicTransformerBlock(nn.Module):
